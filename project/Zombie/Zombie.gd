b1f23e36dfc785294638e6cffba4f98c36fde0e9
Unknown <18maximus8050@gmail.com>
2019-03-17 18:38:52 +0100

    Fixed zombie movement

--- project/Zombie/Zombie.gd
+++ project/Zombie/Zombie.gd
@@ -10,13 +10,15 @@ func _ready():
 	add_to_group("zombies")
 
 func _physics_process(delta):
+	delta *= 60.0
+	
 	if player == null:
 		return
 	
 	var vec_to_player = player.global_position - global_position
 	vec_to_player = vec_to_player.normalized()
 	global_rotation = atan2(vec_to_player.y, vec_to_player.x)
-	move_and_collide(vec_to_player * MOVE_SPEED * delta)
+	move_and_slide(vec_to_player * MOVE_SPEED * delta)
 
 func damage(dmg):
 	health = health - dmg
