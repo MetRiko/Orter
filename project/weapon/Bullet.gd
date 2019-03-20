aaf2f65cfadfaf5b83cdef0eda404ac5fda69527
Przemysław Skoneczny <36925416+Rardan@users.noreply.github.com>
2019-03-20 18:52:22 +0100

    Add files via upload

--- project/weapon/Bullet.gd
+++ project/weapon/Bullet.gd
@@ -2,6 +2,10 @@ extends KinematicBody2D
 
 var vel = Vector2()
 
+func _ready():
+	get_viewport().audio_listener_enable_2d = true
+	$AudioStreamPlayer2D.play()
+
 func setVelocity(vel):
 	self.vel = vel
 	
