e8dc4746be30a296aba24a30a881f5ede60176e9
Rafał Baszak <rafal.baszak@gmail.com>
2019-06-04 12:15:08 +0200

    Todos, random block side moves

--- project/Stages/Tetris/Blocks/Block_Green.gd
+++ project/Stages/Tetris/Blocks/Block_Green.gd
@@ -5,11 +5,11 @@ const MOVEMENT = 192
 var motion = Vector2()
 
 var time = 0
-#var timeSidestep = 0
+var timeSidestep = 0
 #var timeRotation = 0
 var timeMult = 1.0
 var timePaused = false
-#var randSide = null
+var randSide = null
 #var randRot = null
 
 func _ready():
@@ -20,20 +20,20 @@ func _physics_process(delta):
 	motion.x = 0
 	
 	time += delta * timeMult
-#	timeSidestep += delta * timeMult
+	timeSidestep += delta * timeMult
 #	timeRotation += delta * timeMult
 	
-#	if (timeSidestep >= 2 and not timePaused):
-#		randSide = randi()%8+1
-#		if (randSide==1):
-#			motion.x = MOVEMENT
-#		elif (randSide==2):
-#			motion.x = -MOVEMENT
-#		else:
-#			motion.x = 0
-#		move_and_collide(motion)
-#		motion.x = 0
-#		timeSidestep = 0
+	if (timeSidestep >= 2 and not timePaused):			#	10% chance of green block moving sideways
+		randSide = randi()%21+1
+		if (randSide==1):
+			motion.x = MOVEMENT
+		elif (randSide==2):
+			motion.x = -MOVEMENT
+		else:
+			motion.x = 0
+		move_and_collide(motion)
+		motion.x = 0
+		timeSidestep = 0
 	
 #	if (timeRotation >= 3 and not timePaused):
 #		randRot = randi()%10+1
