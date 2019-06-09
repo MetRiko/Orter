ee80c4dfdc3f68f1425213f1ed501d71760b6d04
Rafał Baszak <rafal.baszak@gmail.com>
2019-06-09 21:22:07 +0200

    Added tracks

--- project/Stages/Level.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func addProjectile(node):
-	$Projectiles.add_child(node)
--- project/singletons/Game.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-onready var nRoot = get_tree().get_root().get_node("Root")
-
-func getCurrentStage():
-	return nRoot.get_node("Level")
